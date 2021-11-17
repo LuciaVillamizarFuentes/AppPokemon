8f406c753fba0c43e9774b7142357c1e5de4bb1b
Lucia <adriluvifu@gmail.com>
2021-11-16 22:02:59 -0500

    Lamado API listar pokemons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Pokemons from "./components/Pokemons";
 
 function App() {
   return (
     <div className="App">
+      <Pokemons /> 
       <h1> Hola mundo </h1>
     </div>
   );
